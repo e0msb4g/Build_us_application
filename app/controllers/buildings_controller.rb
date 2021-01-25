bbf8b6c001f523831610084c681c05042d372799
e0msb4g <koki.wind194@gmail.com>
2021-01-26 05:40:05 +0900

    ゲストログイン機能作成

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -6,4 +6,15 @@ class BuildingsController < ApplicationController
       render "buildings/index"
     end 
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.alphanumeric(8)
+      # 大文字、小文字のアルファベット、数字からランダムに８桁のパスワードを生成
+      user.nickname = "ゲスト"
+    end
+    sign_in user
+    redirect_to home_index_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "buildings#index"
+  post '/buildings/guest_sign_in', to: 'buildings#new_guest'
 
   resources :home
 end
